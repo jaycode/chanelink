bcbacb7577008af22994e0d36d54db31cd94772e
jaycode <teguhwpurwanto@gmail.com>
2015-04-06 15:46:16 +0700

    Renamed migration file.

---
+++ webapp/test/unit/property_channel_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class PropertyChannelTest < ActiveSupport::TestCase
+  test "settings" do
+    property_channel = PropertyChannel.new do |p|
+      p.property = properties(:big_hotel_1)
+      p.channel = channels(:ctrip)
+      p.pool = pools(:default_big_hotel_1)
+    end
+    property_channel.settings = {:agoda => {:id => 'agoda-id'}, :ctrip => {:id => 'the-id'}}
+    assert_equal 'the-id', property_channel.settings(:ctrip, :id)
+
+    # These are needed when saving a new property channel.
+    property_channel.skip_channel_specific = true
+    property_channel.skip_rate_conversion_multiplier = true
+
+    property_channel.settings = {:agoda => {:id => 'agoda-id2'}}
+    property_channel.save
+
+    saved_property_channel = PropertyChannel.find(id: property_channel.id).first
+    assert_equal 'agoda-id2', saved_property_channel.settings(:agoda, :id)
+
+    saved_property_channel.destroy_settings
+    assert_equal saved_property_channel.settings(:__default, {})
+  end
+end
