61ee2c62b04a5a1afd257e7d44d1516a7f0f2d39
steven <steven@triodigitalagency.com>
2015-05-19 18:19:44 +0700

    Channel reservation via wash_out/soap_service (still on progress).

--- webapp/app/controllers/api_controller.rb
+++ webapp/app/controllers/api_controller.rb
@@ -1,15 +1,52 @@
 class ApiController < ApplicationController
   soap_service namespace: 'API:Chanelink'
-  # Simple case
-  soap_action "book",
-              :args   => {
-                :channel => :string,
-                :hotel_id => :string,
-                :rate_plan => :string,
-                :nights => :int},
-              :return => :string
-  def book
-    # render :soap => "hello world"
+  
+  # # Simple case
+  # soap_action "book",
+  #   :args => {
+  #     :channel    => :string,
+  #     :hotel_id   => :string,
+  #     :rate_plan  => :string,
+  #     :nights     => :integer
+  #   },
+  #   :return => :string
+
+  # def book
+  #   render :soap => params[:channel]
+  # end
+
+  # Bookings
+  soap_action "Bookings",
+    :args => {
+      :channel    => {
+        :code     => :integer,
+        :username => :string,
+        :password => :string
+      },
+      :hotels     => [
+        :hotel => {
+          :code           => :string,
+          :category       => :string,
+          :rate           => :double,
+          :currency_code  => :string,
+          :start_date     => :string,
+          :end_date       => :string,
+          :rooms          => :integer
+        }
+      ]
+    },
+    :return => {
+      :status     => :string,
+      :trans_code => :string,
+      :date       => :string
+    },
+    :to     => :bookings
+  def bookings
+    render :soap => {
+      :status     => 'success',
+      :trans_code => 'IA123POK',
+      :date       => '19/05/2015 18:04 WIB'  
+    }
   end
 
 end
